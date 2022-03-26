3b8229ee479ccaff02621f22a71eb0d287765d88
bond_307 <freelancer4747.com>
2022-03-26 10:42:34 +0600

    Update with api connection

---
+++ inc/footer.php
@@ -0,0 +1,24 @@
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="location" data-backdrop="static" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="locationLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+
+                <div class="modal-body">
+                    <img src="assets/img/bkash.png" width="100%" height="100%" alt="">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/custom.js"></script>
+<script src="assets/js/check_packeg.js"></script>
+
+</html>
